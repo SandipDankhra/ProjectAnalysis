7d88a5d7f780b4c0b31a6e380b9cafb173079c41
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-04-06 14:37:32 +0530

    StudentPortal

--- AngularApps/StudentInfoApp/src/app/students/student-insert/student-insert.component.ts
+++ AngularApps/StudentInfoApp/src/app/students/student-insert/student-insert.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-student-insert',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-insert.component.css']
 })
 export class StudentInsertComponent implements OnInit {
-
-  constructor() { }
+  StudentinsertGroup: FormGroup;
+  result: any;
+  constructor(private activateRouter: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
+    this.StudentinsertGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      mobileNumber: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      fees: ['', Validators.required],
+    });
   }
 
+  InsertStudent() {
+    this.http.post<any[]>('https://localhost:44391/api/Students', { "name": this.StudentinsertGroup.value.name, "mobileNumber": this.StudentinsertGroup.value.mobileNumber, "emailId": this.StudentinsertGroup.value.email, "password": this.StudentinsertGroup.value.password, "fees": this.StudentinsertGroup.value.fees }).subscribe(t => {
+      this.result = t;
+      alert("Student inserted succesfully");
+    })
+  }
 }
--- AngularApps/StudentInfoApp/src/app/students/student-update/student-update.component.ts
+++ AngularApps/StudentInfoApp/src/app/students/student-update/student-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-student-update',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentUpdateComponent implements OnInit {
 
-  constructor() { }
+  StudentupdateGroup: FormGroup;
+  result: any;
+  sid: any;
+  sname: any;
+  smobileno: any;
+  semail: any;
+  spassword: any;
+  sfees: any;
 
-  ngOnInit() {
+  constructor(private activateRouter: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private http: HttpClient) {
+
   }
 
+  ngOnInit() {
+    this.sid = this.activateRouter.snapshot.paramMap.get("id");
+    this.http.get('https://localhost:44391/api/Students' + "/" + this.sid).subscribe(t => {
+      this.result = t;
+      const data = this.result[0];
+
+      this.sname = data.name;
+      this.semail = data.emailId;
+      this.spassword = data.password
+      this.smobileno = data.mobileNo
+      this.sfees = data.mobileNo
+
+
+      this.StudentupdateGroup = this.formBuilder.group({
+        name: [this.sname, Validators.required],
+        mobileNumber: [this.smobileno, Validators.required],
+        email: [this.semail, Validators.required],
+        password: [this.spassword, Validators.required],
+        fees: [this.sfees, Validators.required],
+      })
+
+    })
+  }
+  
+  UpdateStudent() {
+    this.http.post<any[]>('https://localhost:44391/api/Students', { "name": this.StudentupdateGroup.value.name, "mobileNumber": this.StudentupdateGroup.value.mobileNumber, "emailId": this.StudentupdateGroup.value.email, "password": this.StudentupdateGroup.value.password, "fees": this.StudentupdateGroup.value.fees }).subscribe(t => {
+      this.result = t;
+      alert("Student Updates succesfully");
+    })
+  }
 }
